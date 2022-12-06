432dba129f7b031c2dcc850a5f0d856afa5fe0e4
0x41414141 <gourisariaanjani@gmail.com>
2022-12-07 03:44:58 +0530

    ...

--- local.py
+++ local.py
@@ -6,3 +6,5 @@ try:
     print("!git clone https://"+sys.argv[1]+":x-oauth-basic@github.com/"+sys.argv[2]+"/"+repo.name+".git")
 except IndexError:
      print("Usage: ./"+sys.argv[0]+" <Personal_Token> <Username>")
+except github.GithubException.BadCredentialsException:
+     print("")
