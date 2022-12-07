804a34db5c923e529ce6bfa6717819791f06cbb0
0x41414141 <gourisariaanjani@gmail.com>
2022-12-07 11:23:12 +0530

    ...

--- local.py
+++ local.py
@@ -3,6 +3,8 @@ import sys
 try:
   g = Github(sys.argv[1])
   for repo in g.get_user().get_repos():
-    print("!git clone https://"+sys.argv[1]+":x-oauth-basic@github.com/"+sys.argv[2]+"/"+repo.name+".git")
+    f = open("githun.sh", "a")
+    f.write("git clone https://"+sys.argv[1]+":x-oauth-basic@github.com/"+sys.argv[2]+"/"+repo.name+".git\n")
+    f.close()
 except IndexError:
-     print("Usage: ./"+sys.argv[0]+" <Personal_Token> <Username>")
+    print("Usage: ./"+sys.argv[0]+" <Personal_Token> <Username>")
